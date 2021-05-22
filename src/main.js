fd69e9c3e6038f1f77d108b8e3d0637c43834e1c
m-alghobary <m.alghobary90@gmail.com>
2021-05-22 10:39:37 +0300

    refactor to optional chaining

--- src/main.js
+++ src/main.js
@@ -2,15 +2,11 @@ import * as THREE from 'three';
 import init from './core/init';
 import { loop } from './core/utils';
 
-const enableControls = true;
-
-const { scene, camera, renderer, controls } = init({
-	withControls: enableControls,
-});
+const { scene, camera, renderer, controls } = init();
 
 loop(() => {
 	// Update Orbital Controls
-	if (enableControls) controls.update();
+	controls?.update();
 
 	// Render
 	renderer.render(scene, camera);
